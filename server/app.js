bbf6c2898bc168d9147e9e5c78ce7d5f81672c8d
José Pedro <josepedroteles@gmail.com>
2018-04-21 17:34:08 +0100

    remove listen

--- server/app.js
+++ server/app.js
@@ -46,10 +46,6 @@ app.use(function(err, req, res, next) {
     res.render('error');
 });
 
-app.listen(3000, function(req, res) {
-    console.log("Server is listening on port 3000");
-});
-
 mongoose.connect(dbConfig.url)
     .then(() => {
         console.log("Successfully connected to the database");
