7277669f11f9243ab12b227b3bcf8928357059a4
Daniel Maciel <danmaciell@live.com.pt>
2018-04-21 20:21:42 +0100

    Pedido ao PortugalOdds

---
+++ server/request/portugalodds.js
@@ -0,0 +1,30 @@
+const rp = require('request-promise');
+const fs = require('fs');
+
+function getGames() {
+    console.log("Entrando no getGames");
+    return rp({
+            method: 'GET',
+            uri: 'https://still-reef-29535.herokuapp.com/api/games/',
+            json: true,
+            simple: true
+        }).then((response) => {
+            return new Promise(function (resolve, reject) {
+                console.log(response);
+                fs.writeFile('resources/games.json', JSON.stringify(response), (err) => {
+                    if (err) {
+                        reject(err);
+                    } else {
+                        resolve(response)
+                    }
+                })
+            })
+        })
+        .catch((err) => {
+            return Promise.reject(err);
+        })
+}
+
+module.exports = {
+    getGames
+}
